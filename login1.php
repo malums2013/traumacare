ab4c251df7c4c073ab3d1c2e800c2c76b0430beb
malums2013 <42314304+malums2013@users.noreply.github.com>
2018-11-12 14:15:43 +0530

    login page
    added login

---
+++ login1.php
@@ -0,0 +1,74 @@
+
+<!--
+Author: W3layouts
+Author URL: http://w3layouts.com
+License: Creative Commons Attribution 3.0 Unported
+License URL: http://creativecommons.org/licenses/by/3.0/
+-->
+<!DOCTYPE HTML>
+<html>
+	<head>
+		<title>medicare Website Template | 404 :: W3layouts</title>
+		<link href="css/style.css" rel="stylesheet" type="text/css"  media="all" />
+		<link href='http://fonts.googleapis.com/css?family=Ropa+Sans' rel='stylesheet' type='text/css'>
+	</head>
+	<body>
+		<!--start-wrap-->
+		
+			<!--start-header-->
+			<div class="header">
+				<div class="wrap">
+				<!--start-logo-->
+				<div class="logo">
+					<a href="index.php"><img src="images/logo.png" title="logo" /></a>
+				</div>
+				<!--end-logo-->
+				<!--start-top-nav-->
+				<div class="top-nav">
+					<ul>
+						<li class="active"><a href="index.php">Home</a></li>
+						<li><a href="about.php">About</a></li>
+						<li><a href="services.php">services</a></li>
+                                                <li><a href="login1.php">login</a></li>
+                                                <li><a href="register.php">register now</a></li>
+					</ul>					
+				</div>
+				<div class="clear"> </div>
+				<!--end-top-nav-->
+			</div>
+			<!--end-header-->
+		</div>
+		    <div class="clear"> </div>
+		   <div class="wrap">
+		   	<div class="erroe-page">
+		   		<h3>LOGIN</h3>
+                                <h2><font color="grey"> <form method="POST">
+            USER NAME: <input type="text" name="user"/><BR>
+            PASSWORD : <input type="password" name="pass"/><br>
+            <font color="grey"><input type="submit" name="sub" value="LOGIN"/>
+            
+            </font> </form></font></h2>
+		   	</div>
+		   </div>
+		   <div class="clear"> </div>
+		   <div class="footer">
+		   	 <div class="wrap">
+		   	<div class="footer-left">
+		   			<ul>
+						<li><a href="index.php">Home</a></li>
+						<li><a href="about.php">About</a></li>
+						<li><a href="services.php">services</a></li>
+                                                <li><a href="login.php">login</a></li>
+                                                <li><a href="register.php">register now</a></li>
+					</ul>
+		   	</div>
+		   	<div class="footer-right">
+		   		<p>Medicare | Design By <a href="http://w3layouts.com/">W3Layouts</a></p>
+		   	</div>
+		   	<div class="clear"> </div>
+		   </div>
+		   </div>
+		<!--end-wrap-->
+	</body>
+</html>
+
